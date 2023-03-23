e9128e5f4ca9b07f3332d88869802506d829e4f8
dheeraj <dheerajck18@gmail.com>
2023-03-23 08:19:28 +0530

    Refactored load_nation.py

--- scripts/load_nation.py
+++ scripts/load_nation.py
@@ -3,56 +3,69 @@ import zipfile
 from pathlib import Path
 
 from dotenv import load_dotenv
-
 from geocoder import Database
 
-from .helpers import download, run_shp2pgsql, clear_temp
-
+from .helpers import clear_temp, download, run_shp2pgsql
 
 load_dotenv(".env")
 
 GISDATA_FOLDER = os.getenv("GISDATA_FOLDER")
 YEAR = os.getenv("YEAR")
-
 SHP2PGSQL = os.getenv("SHP2PGSQL")
 
+# Define paths
 GISDATA_FOLDER = Path(GISDATA_FOLDER)
 TEMP_DIR = Path(f"{GISDATA_FOLDER}/temp/")
-
 BASE_PATH = f"www2.census.gov/geo/tiger/TIGER{YEAR}"
 BASE_URL = f"https://{BASE_PATH}"
 
 
-def load_national_data_caller():
+def download_extract(country, section):
     current_working_directory = os.getcwd()
-
-    # Download and extract state data
     os.chdir(GISDATA_FOLDER)
 
-    state_url = f"{BASE_URL}/STATE/tl_{YEAR}_us_state.zip"
+    current_url = f"{BASE_URL}/{section.upper()}/tl_{YEAR}_{country}_{section}.zip"
 
-    # zip_file_path = "www2.census.gov/geo/tiger/TIGER2022/STATE/tl_2022_us_state.zip"
-    zip_file_path = download(state_url)
+    download(current_url)
+
     clear_temp(TEMP_DIR)
+    os.chdir(f"{GISDATA_FOLDER}/{BASE_PATH}/{section.upper()}")
+    for z in os.listdir(os.getcwd()):
+        if z.startswith(f"tl_{YEAR}_{country}") and z.endswith(f"_{section}.zip"):
+            with zipfile.ZipFile(z) as place_zip:
+                place_zip.extractall(TEMP_DIR)
 
-    # Create schema
+    os.chdir(current_working_directory)
+
+
+def load_national_data():
     db = Database()
+
+    # Everything inside this function require temp dir as working directory
+    # All functions thats called from this function that needs to change working directory change it at start and change back at end
+    os.chdir(TEMP_DIR)
+
+    ###############
+    # State
+    ###############
+
+    section = "state"
+    country = "us"
+
+    start_message = f"Started to setup {section}"
+    print(start_message)
+    download_extract(country, section)
 
     db.execute("DROP SCHEMA IF EXISTS tiger_staging CASCADE;")
     db.execute("CREATE SCHEMA tiger_staging;")
 
-    os.chdir(f"{BASE_PATH}/STATE")
-    # Load data into staging table
-    for z in os.listdir(os.getcwd()):
-        if z.startswith("tl_") and z.endswith("state.zip"):
-            with zipfile.ZipFile(z) as state_zip:
-                state_zip.extractall(TEMP_DIR)
-
-    # exit()
-    os.chdir(TEMP_DIR)
-
     db.execute(
-        "CREATE TABLE IF NOT EXISTS tiger_data.state_all(CONSTRAINT pk_state_all PRIMARY KEY (statefp),CONSTRAINT uidx_state_all_stusps  UNIQUE (stusps), CONSTRAINT uidx_state_all_gid UNIQUE (gid) ) INHERITS(tiger.state); "
+        """CREATE TABLE IF NOT EXISTS tiger_data.state_all(
+            CONSTRAINT pk_state_all PRIMARY KEY (statefp),
+            CONSTRAINT uidx_state_all_stusps  UNIQUE (stusps), 
+            CONSTRAINT uidx_state_all_gid UNIQUE (gid)
+        ) INHERITS(tiger.state);
+        """
     )
 
     command = f"shp2pgsql -D -c -s 4269 -g the_geom -W 'latin1' tl_{YEAR}_us_state.dbf tiger_staging.state"
@@ -63,30 +76,23 @@ def load_national_data_caller():
     db.execute(
         "CREATE INDEX IF NOT EXISTS tiger_data_state_all_the_geom_gist ON tiger_data.state_all USING gist(the_geom);"
     )
+
     db.execute("VACUUM ANALYZE tiger_data.state_all")
+    print(f"{start_message} - Done\n")
 
-    # Download and extract county data
-    os.chdir(GISDATA_FOLDER)
-    county_url = f"{BASE_URL}/COUNTY/tl_{YEAR}_us_county.zip"
+    ###############
+    # County
+    ###############
 
-    # zip_file_path = "www2.census.gov/geo/tiger/TIGER2022/COUNTY/tl_2022_us_county.zip"
-    zip_file_path = download(county_url)
-    # os.chdir(os.path.join(GISDATA_FOLDER, "COUNTY"))
-    os.chdir(f"{BASE_PATH}/COUNTY")
+    section = "county"
+    country = "us"
 
-    clear_temp(TEMP_DIR)
+    start_message = f"Started to setup {section}"
+    print(start_message)
+    download_extract(country, section)
 
-    # Create schema
     db.execute("DROP SCHEMA IF EXISTS tiger_staging CASCADE;")
     db.execute("CREATE SCHEMA tiger_staging;")
-
-    # Load data into staging table
-    for z in os.listdir(os.getcwd()):
-        if z.startswith("tl_") and z.endswith("county.zip"):
-            with zipfile.ZipFile(z) as county_zip:
-                county_zip.extractall(TEMP_DIR)
-
-    os.chdir(TEMP_DIR)
 
     db.execute(
         """
@@ -106,8 +112,6 @@ def load_national_data_caller():
     command = f"shp2pgsql -D -c -s 4269 -g the_geom -W 'latin1' tl_{YEAR}_us_county.dbf tiger_staging.county"
     run_shp2pgsql(command)
 
-    # Rename column geoid to cntyidfp and load staged data
-
     db.execute(
         """
         ALTER TABLE tiger_staging.county RENAME geoid TO cntyidfp;
@@ -119,31 +123,38 @@ def load_national_data_caller():
         """
         CREATE INDEX IF NOT EXISTS tiger_data_county_the_geom_gist ON tiger_data.county_all USING gist(the_geom);
         CREATE UNIQUE INDEX IF NOT EXISTS uidx_tiger_data_county_all_statefp_countyfp ON tiger_data.county_all USING btree(statefp,countyfp);
-    """
+        """
     )
-
-    # Create table tiger_data.county_all_lookup
-
-    db.execute(
-        "CREATE TABLE tiger_data.county_all_lookup ( CONSTRAINT pk_county_all_lookup PRIMARY KEY (st_code, co_code)) INHERITS (tiger.county_lookup);"
-    )
-
-    # Vacuum analyze tables
 
     db.execute("VACUUM ANALYZE tiger_data.county_all;")
-    db.execute("VACUUM ANALYZE tiger_data.county_all_lookup;")
+
+    # Create table tiger_data.county_all_lookup
+    db.execute(
+        """
+        CREATE TABLE tiger_data.county_all_lookup(
+            CONSTRAINT pk_county_all_lookup PRIMARY KEY (st_code, co_code)
+        ) INHERITS (tiger.county_lookup)
+        """
+    )
 
     # Insert data into tiger_data.county_all_lookup
-
     db.execute(
         """
         INSERT INTO tiger_data.county_all_lookup (st_code, state, co_code, name)
         SELECT CAST(s.statefp as integer), s.abbrev, CAST(c.countyfp as integer), c.name
         FROM tiger_data.county_all AS c
         INNER JOIN state_lookup AS s ON s.statefp = c.statefp;
-    """
+        """
     )
 
-    # Commit changes and close the connection
+    # Vacuum analyze tables
+    db.execute("VACUUM ANALYZE tiger_data.county_all_lookup;")
+    print(f"{start_message} - Done\n")
 
+
+def load_national_data_caller():
+    print("\n\nAdding US national data")
+
+    current_working_directory = os.getcwd()
+    load_national_data()
     os.chdir(current_working_directory)
